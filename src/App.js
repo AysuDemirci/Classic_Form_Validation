ce14c6e4ba468e4df5b8309978a16e565b81442e
Aysu <aysu.gunerr@gmail.com>
2022-11-14 23:33:31 +0300

    added some styles to the form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./style.css";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="landing-page">
         <FormValidation/>
       </div>
     );
